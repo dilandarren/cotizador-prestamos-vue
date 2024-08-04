b0b72e2b6dc1e7bbc1a94a47ffba6754b34e227e
Dilan <dilandarren1@gmail.com>
2024-08-04 22:23:44 +0200

    add ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup>
+    import { ref, reactive } from 'vue'
     import Header from './components/Header.vue'
+
+    const cantidad = ref(0);
+
+    const state = reactive({
+      cantidad: 0
+    })
+
+    function handleChange(e) {
+      console.log(e.target.value);
+    }
+
+    console.log(cantidad.value); //REF
+
+    console.log(state.value); //REACTIVE
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="my-5">
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+      >
+    </div>
   </div>
 </template>
