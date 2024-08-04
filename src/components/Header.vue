345ee3d33e397849fba71ebe666a5fbb772b8d16
Dilan <dilandarren1@gmail.com>
2024-08-04 22:07:59 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuanto <span class="text-indigo-600">dinero</span> necesitas?
+        </h1>
+    </div>
+</template>
+
