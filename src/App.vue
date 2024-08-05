631efcf161e0168d1bfe43fc93d3008995c2a982
Dilan <dilandarren1@gmail.com>
2024-08-05 21:25:51 +0200

    fix project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,109 @@
 <script setup>
-    import { ref, reactive } from 'vue'
-    import Header from './components/Header.vue'
+    import { ref, computed, handleError } from 'vue';
+    import Header from './components/Header.vue';
+    import Button from './components/Button.vue';
+    
 
-    const cantidad = ref(0);
+    const cantidad = ref(10000);
+    const meses = ref(6)
+    const MIN = 0;
+    const MAX = 20000
+    const STEP = 100
 
-    const state = reactive({
-      cantidad: 0
-    })
+    const formatearDinero = computed(() => {
+      const formatter = new Intl.NumberFormat('es-ES', {
+        style:'currency',
+        currency: 'EUR'
+      });
+      return formatter.format(cantidad.value);
+    });
 
-    function handleChange(e) {
-      console.log(e.target.value);
+    const handleChangeDecremento = () => {
+      const valor = cantidad.value - STEP;
+      if(valor < MIN) {
+        alert('Cantidad no valida');
+        return
+      }
+      cantidad.value = valor;
+    }
+    const handleChangeIncremento = () => {
+      const valor = cantidad.value + STEP;
+      if(valor > MAX) {
+        alert('Cantidad no valida');
+        return
+      }
+      cantidad.value = valor;
     }
 
-    console.log(cantidad.value); //REF
 
-    console.log(state.value); //REACTIVE
+    // function handleChange(e) {
+    //   // cantidad.value = Number(e.target.value);
+    //   cantidad.value = Number(e.target.value);
+    // }
+
+    // console.log(cantidad.value); //REF
+
+    // console.log(state.value); //REACTIVE
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+    
+    <div class="flex justify-between mt-10">
+      <Button 
+      :operador="'-'"
+      @fn="handleChangeDecremento"
+      />
+
+
+      <Button 
+      :operador="'+'"
+      @fn="handleChangeIncremento"
+      />
+
+
+    </div>
 
     <div class="my-5">
       <input 
         type="range"
         class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
-        @input="handleChange"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        v-model.number="cantidad"
+      />
+
+      <p class="text-center my-10 text-5xl font font-extrabold text-indigo-600"> 
+        {{formatearDinero}}
+      </p>
+
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Elije un <span class="text-indigo-600">Plazo</span> a pagar
+      </h2>
+
+      <select
+        class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-5"
+        :value="meses"
+        v-model.number="meses"
       >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+
+      <!-- <p v-text="`${cantidad}`"></p> -->
+    </div>
+
+    <div class="my-5 space-y-3 bg-gray-50 p-5">
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Resumen <span class="text-indigo-600">de pagos</span>
+      </h2>
+
+      <p class="text-xl text-gray-500 text-center font-bold">{{meses}}Meses</p>
+      <p class="text-xl text-gray-500 text-center font-bold">Total a pagar</p>
+      <p class="text-xl text-gray-500 text-center font-bold">Mensuales</p>
     </div>
   </div>
 </template>
